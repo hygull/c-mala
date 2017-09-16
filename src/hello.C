f1782a0d6f78d7a0557d886f75732675392519b1
Rishikesh_67 <rishikesh0014051992@gmail.com>
2017-09-16 08:36:46 +0530

    1 created hello.C

---
+++ src/hello.C
@@ -0,0 +1,16 @@
+/**
+	{
+		"created_on": "16 September 2017",
+		"aim": "printing messages on console",
+		"coded_by": "Rishikesh Agrawani"
+	}
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello programmers\n");
+	printf("%s\n", "Nikhitha & Malinikesh both are programmers");
+	return 0;
+}
