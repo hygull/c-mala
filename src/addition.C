ccd005e9b536a66e0ddfb62c141eba6acff9b4bb
Rishikesh_67 <rishikesh0014051992@gmail.com>
2017-09-16 08:42:40 +0530

    2 added addition.C

---
+++ src/addition.C
@@ -0,0 +1,17 @@
+/**
+	{
+		"created_on": "16 September 2017",
+		"aim": "adding 2 integers",
+		"coded_by": "Rishikesh Agrawani"
+	}
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	int a=19, b=9;
+
+	printf("%d + %d = %d\n", a, b, a+b );
+	return 0;
+}
