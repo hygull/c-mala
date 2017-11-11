e57691b708ad3f0877db6da8a67200786d7ef453
Rishikesh_67 <rishikesh0014051992@gmail.com>
2017-11-11 13:40:26 +0530

    14 increment operators

---
+++ src/dca/14_increment_operators.C
@@ -0,0 +1,34 @@
+/*
+  Date of creation: 11 November 2017,
+  Title: Write a program to use increment operator,
+  Coded by: Rishikesh Agrawani
+*/
+
+#include<stdio.h>
+
+void main() 
+{
+  int a = 10;
+  int b = 5;
+  int c;
+  
+  // Using post-increment operator
+  // first assign current value 10 then increment
+  c = a++;   
+  printf("%d, %d", a, c);
+  printf("\n");
+  
+  // Using pre-increment operator
+  // first increment then assign incremented value
+  c = ++b;
+  printf("%d, %d\n", b, c);
+  
+  c = ++b;
+  printf("%d, %d", b, c);
+}
+
+/* Output:-
+11, 10
+6, 6
+7, 7
+*/
