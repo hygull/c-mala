885b476f57d667b866645b32ebe366c6936306fd
Rishikesh_67 <rishikesh0014051992@gmail.com>
2017-11-11 11:00:05 +0530

    5 subracting 2 integers

---
+++ src/dca/7_subracting_2_integers.C
@@ -0,0 +1,21 @@
+/*
+  Date of creation: 11 November 2017,
+  Title: Write a program for subtracting one integer number from other,
+  Coded by: Rishikesh Agrawani
+*/
+
+#include<stdio.h>
+
+void main() 
+{
+    int a = 10;    
+    int b = 5;
+    int c;
+    c = a - b;
+    
+    printf("%d - %d = %d", a, b, c);
+}
+
+/* Output:-
+10 - 5 = 5
+*/
